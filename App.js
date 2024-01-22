89fdb7f3da2527c2255baa796d6d25365f40fdd4
Laelatulhidayah <laelatulhidayahela@gmail.com>
2024-01-22 11:58:33 +0700

    menambah fungsi edit dan delete

--- App.js
+++ App.js
@@ -3,29 +3,53 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './components/Home';
 import DataSantri from './components/DataSantri';
 import InputSantriBaru from './components/InputSantriBaru';
-
+import { Ionicons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName=" Home"
-         screenOptions={{
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
           headerStyle: {
-            backgroundColor: '#66CDAA', // Warna latar belakang header
+            backgroundColor: '#66CDAA',
           },
-          headerTintColor: '#fff', // Warna teks untuk tombol back dan title
+          headerTintColor: '#fff',
           headerTitleStyle: {
-            fontWeight: 'bold', // Style untuk judul teks
+            fontWeight: 'bold',
           },
         }}
-        >
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Input Santri Baru" component={InputSantriBaru} />
-        <Drawer.Screen name="Data Santri" component={DataSantri}  />
+      >
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" size={20} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Input Santri Baru"
+          component={InputSantriBaru}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person-add" size={20} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Data Santri"
+          component={DataSantri}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="list" size={20} color={color} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
--- components/DataSantri.js
+++ components/DataSantri.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, TextInput, Button } from 'react-native';
 import { db } from '../config';
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, update, remove } from 'firebase/database';
+import { Ionicons } from '@expo/vector-icons';
 
 const DataSantri = () => {
     const [data, setData] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [editData, setEditData] = useState({
+        id: '',
+        idYayasan: '',
+        namaSantri: '',
+        kamar: '',
+        namaWali: '',
+    });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -12,7 +21,7 @@ const DataSantri = () => {
             onValue(santriRef, (snapshot) => {
                 const dataArr = [];
                 snapshot.forEach((childSnapshot) => {
-                    dataArr.push(childSnapshot.val());
+                    dataArr.push({ id: childSnapshot.key, ...childSnapshot.val() });
                 });
                 setData(dataArr);
             });
@@ -21,33 +30,105 @@ const DataSantri = () => {
         fetchData();
     }, []);
 
+    const toggleEditMode = (item) => {
+        setEditMode(!editMode);
+        setEditData({
+            id: item.id,
+            idYayasan: item.idYayasan,
+            namaSantri: item.namaSantri,
+            kamar: item.kamar,
+            namaWali: item.namaWali,
+        });
+    };
+
+    const saveEdit = () => {
+        const santriRef = ref(db, 'Data Santri/' + editData.id);
+        update(santriRef, {
+            idYayasan: editData.idYayasan,
+            namaSantri: editData.namaSantri,
+            kamar: editData.kamar,
+            namaWali: editData.namaWali,
+        });
+        setEditMode(false);
+    };
+
+    const deleteData = (id) => {
+        const santriRef = ref(db, 'Data Santri/' + id);
+        remove(santriRef);
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Data dari Firebase:</Text>
             <FlatList
                 data={data}
-                keyExtractor={(item) => item.idYayasan.toString()}
+                keyExtractor={(item) => (item && item.idYayasan ? item.idYayasan.toString() : '')}
                 renderItem={({ item }) => (
-                    <View style={styles.row}>
-                        <View style={styles.column}>
-                            <Text style={styles.label}>ID Yayasan:</Text>
-                            <Text>{item.idYayasan}</Text>
+                    <View style={[styles.row, { backgroundColor: 'white', elevation: 5, shadowColor: 'black', shadowOpacity: 0.2, shadowRadius: 5, shadowOffset: { width: 0, height: 4 }}]}>
+                        <View style={styles.dataContainer}>
+                            <View style={styles.column}>
+                                <Text style={styles.label}>ID Yayasan:</Text>
+                                <Text>{item.idYayasan}</Text>
+                            </View>
+
+                            <View style={styles.column}>
+                                <Text style={styles.label}>Nama Santri:</Text>
+                                <Text>{item.namaSantri}</Text>
+                            </View>
+
+                            <View style={styles.column}>
+                                <Text style={styles.label}>Kamar:</Text>
+                                <Text>{item.kamar}</Text>
+                            </View>
+
+                            <View style={styles.column}>
+                                <Text style={styles.label}>Nama Wali:</Text>
+                                <Text>{item.namaWali}</Text>
+                            </View>
                         </View>
 
-                        <View style={styles.column}>
-                            <Text style={styles.label}>Nama Santri:</Text>
-                            <Text>{item.namaSantri}</Text>
+                        <View style={styles.actionsContainer}>
+                            <TouchableOpacity style={styles.editButton} onPress={() => toggleEditMode(item)}>
+                                <View style={styles.buttonContent}>
+                                    <Ionicons name="create" size={20} color="white" style={styles.icon} />
+                                    <Text style={styles.buttonText}>Edit</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.deleteButton} onPress={() => deleteData(item.id)}>
+                                <View style={styles.buttonContent}>
+                                    <Ionicons name="trash-outline" size={20} color="white" style={styles.icon} />
+                                    <Text style={styles.buttonText}>Delete</Text>
+                                </View>
+                            </TouchableOpacity>
                         </View>
 
-                        <View style={styles.column}>
-                            <Text style={styles.label}>Kamar:</Text>
-                            <Text>{item.kamar}</Text>
-                        </View>
-
-                        <View style={styles.column}>
-                            <Text style={styles.label}>Nama Wali:</Text>
-                            <Text>{item.namaWali}</Text>
-                        </View>
+                        {editMode && editData.id === item.id ? (
+                            // Formulir edit
+                            <View style={styles.editForm}>
+                                <TextInput
+                                    placeholder="ID Yayasan"
+                                    value={editData.idYayasan}
+                                    onChangeText={(text) => setEditData({ ...editData, idYayasan: text })}
+                                />
+                                <TextInput
+                                    placeholder="Nama Santri"
+                                    value={editData.namaSantri}
+                                    onChangeText={(text) => setEditData({ ...editData, namaSantri: text })}
+                                />
+                                <TextInput
+                                    placeholder="Kamar"
+                                    value={editData.kamar}
+                                    onChangeText={(text) => setEditData({ ...editData, kamar: text })}
+                                />
+                                <TextInput
+                                    placeholder="Nama Wali"
+                                    value={editData.namaWali}
+                                    onChangeText={(text) => setEditData({ ...editData, namaWali: text })}
+                                />
+                                <Button title="Simpan" onPress={saveEdit} />
+                            </View>
+                        ) : null}
 
                         <View style={styles.separator} />
                     </View>
@@ -67,7 +148,30 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
     },
     row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         marginBottom: 10,
+        padding: 10, // Tambahkan padding ke setiap baris data
+        borderRadius: 5, // Tambahkan border radius untuk sudut yang lebih lembut
+    },
+    dataContainer: {
+        flex: 1,
+    },
+    actionsContainer: {
+        flexDirection: 'row',
+    },
+    editButton: {
+        marginRight: 10,
+        backgroundColor: '#66CDAA',
+        padding: 5,
+        borderRadius: 15,
+        height: 30,
+    },
+    deleteButton: {
+        backgroundColor: 'lightcoral',
+        padding: 5,
+        borderRadius: 15,
+        height: 30,
     },
     column: {
         flexDirection: 'row',
@@ -78,10 +182,27 @@ const styles = StyleSheet.create({
         paddingRight: 5,
     },
     separator: {
-        height: 1,
-        backgroundColor: '#dddddd',
+        height: 10,
+        backgroundColor: '#000',
         marginTop: 10,
         marginBottom: 10,
+    },
+    editForm: {
+        marginTop: 10,
+    },
+    buttonContent: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    icon: {
+        marginRight: 5,
+        alignSelf: 'center',
+    },
+    buttonText: {
+        fontSize: 14,
+        color: 'white',
     },
 });
 
